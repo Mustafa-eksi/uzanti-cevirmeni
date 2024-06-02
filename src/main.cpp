13d0681919dc3b3f023971999e50829efab8ef66
mustafa <mustafa.eskieksi@gmail.com>
2024-06-02 13:33:23 +0300

    Added multifile conversion
    Signed-off-by: mustafa <mustafa.eskieksi@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,11 @@ GtkFileDialog *fd;
 GCancellable *cancel;
 GtkWidget *dd;
 GtkWidget *convert_button;
-char *path;
+std::vector<char*> paths;
 enum ConverterProgram cp;
 GtkWidget *sf_button, *filepath_label;
 GtkAlertDialog* dialg;
+GtkWidget *filepath_view;
 
 void alert_clicked(GObject *source_object, GAsyncResult *res, gpointer data)
 {
@@ -33,16 +34,32 @@ void show_alert(const char* text, const char* detail) {
 
 void async_callback(GObject *source_object, GAsyncResult *res, gpointer data)
 {
-	GError *err;
-	GFile *a = gtk_file_dialog_open_finish(fd, res, &err);
-	path = g_file_get_path(a);
-	if (!path) return;
-	char *ext = get_extension(path);
-	cp = (enum ConverterProgram) get_converter_from_extension(ext);
-	check_available(&cp);
-	//printf("converter: '%s'\n", ext);
-	GtkStringList *strlist;
 	char* buff[BIG_BUFF] = {};
+	GtkStringList *strlist;
+	GError *err;
+
+	auto files = gtk_file_dialog_open_multiple_finish(fd, res, &err);
+	auto count = g_list_model_get_n_items(files);
+
+	paths.clear();
+	cp = UNSUPPORTED;
+	for (size_t i = 0; i < count; i++) {
+		auto gfile = (GFile*) g_list_model_get_item(files, i);
+		if (!gfile)
+			continue;
+		auto filepath = g_file_get_path(gfile);
+		//printf("file %ld: %s\n", i, filepath);
+		paths.push_back(filepath);
+
+		char *ext = get_extension(filepath);
+		enum ConverterProgram tmp = (enum ConverterProgram) get_converter_from_extension(ext);
+		check_available(&tmp);
+		if (cp == UNSUPPORTED)
+			cp = tmp;
+		else if (cp != tmp)
+			continue;
+	}
+	paths.push_back(NULL);
 	if ((cp & IMAGEMAGICK) != 0) {
 		create_dropdown_list(imagemagick_all, buff);
 	} else if ((cp & FFMPEG) != 0) {
@@ -52,16 +69,20 @@ void async_callback(GObject *source_object, GAsyncResult *res, gpointer data)
 	}*/ else if ((cp & PANDOC) != 0) {
 		create_dropdown_list(pandoc_all, buff);
 	} else {
-		show_alert("Bu dosya formatı için hiçbir çevirici program bulunamadı: %s", ext);
+		show_alert("Seçilmiş hiçbir dosya için çevirmen program bulunamadı!", NULL);
 		return;
 	}
+
 	strlist = gtk_string_list_new(buff);
 	gtk_drop_down_set_model(GTK_DROP_DOWN(dd), G_LIST_MODEL(strlist));
 	gtk_widget_set_visible(dd, true);
 	gtk_widget_set_visible(convert_button, true);
 	gtk_button_set_label(GTK_BUTTON(sf_button), "Dosyayı değiştirin");
 	gtk_widget_set_visible(filepath_label, true);
-	gtk_label_set_text(GTK_LABEL(filepath_label), path);
+
+	auto filepath_strlist = gtk_string_list_new(&paths[0]);
+	auto filepath_model = gtk_no_selection_new(G_LIST_MODEL(filepath_strlist));
+	gtk_list_view_set_model(GTK_LIST_VIEW(filepath_view), GTK_SELECTION_MODEL(filepath_model));
 	//free(ext);
 }
 
@@ -81,11 +102,20 @@ void convert_clicked(GtkWidget *widget, gpointer data)
 	guint selected = gtk_drop_down_get_selected(GTK_DROP_DOWN(dd));
 	GtkStringList *model = GTK_STRING_LIST(gtk_drop_down_get_model(GTK_DROP_DOWN(dd)));
 	const char *ext = gtk_string_list_get_string(model, selected);
-	enum Result res = convert_helper(path, ext, cp);
+	enum Result res = SUCCESS;
+	// TODO: Make this multithreaded.
+	for (const auto& path : paths) {
+		if (!path)
+			continue;
+		auto r = convert_helper(path, ext, cp);
+		if (res == SUCCESS)
+			res = r;
+		printf("converted: %s\n", path);
+	}
 	if (res == SUCCESS) {
-		show_alert("Dosya başarıyla çevrildi!", NULL);
+		show_alert("Bütün dosyalar başarıyla çevrildi!", NULL);
 	} else {
-		show_alert("Dosya çevrimi başarısız oldu: %s", result_to_str(res));
+		show_alert("Bazı dosyaların çevrimi başarısız oldu!: %s", result_to_str(res));
 	}
 }
 
@@ -129,7 +159,7 @@ void clicked(GtkWidget *widget, gpointer data)
 	g_list_store_append(gls, filter_doc);
 
 	gtk_file_dialog_set_filters(fd, G_LIST_MODEL(gls));
-	gtk_file_dialog_open(fd, GTK_WINDOW(window), cancel, async_callback, NULL);
+	gtk_file_dialog_open_multiple(fd, GTK_WINDOW(window), cancel, async_callback, NULL);
 }
 
 void dd_setup(GtkSignalListItemFactory* self, GObject* object, gpointer user_data) {
@@ -172,9 +202,13 @@ static void activate(GtkApplication *app, gpointer user_data)
 	gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
 	gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
 
-	filepath_label = gtk_label_new(NULL);
-	gtk_widget_set_visible(filepath_label, false);
-	gtk_box_append(GTK_BOX(box), filepath_label);
+	/*auto filepath_strlist = gtk_string_list_new(&paths[0]);
+	auto filepath_model = gtk_single_selection_new(G_LIST_MODEL(filepath_strlist));*/
+	auto filepath_factory = gtk_signal_list_item_factory_new();
+	g_signal_connect(filepath_factory, "setup", G_CALLBACK(dd_setup), NULL);
+	g_signal_connect(filepath_factory, "bind", G_CALLBACK(dd_bind), NULL);
+	filepath_view = gtk_list_view_new(NULL, filepath_factory);
+	gtk_box_append(GTK_BOX(box), filepath_view);
 
 	sf_button = gtk_button_new();
 	gtk_widget_set_valign(sf_button, GTK_ALIGN_CENTER);
