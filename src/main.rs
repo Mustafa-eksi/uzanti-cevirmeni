e3f9ba0adb8def8e4444c3a2b5a3efa409de6f27
mustafa <mustafa.eskieksi@gmail.com>
2024-07-22 01:28:14 +0300

    Removing files from list is still broken

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::borrow::BorrowMut;
 use std::sync::{Arc, Mutex};
+use std::cell::RefCell;
 use gtk::gio::builders::ListStoreBuilder;
 use gtk::gio::{Cancellable, ListModel};
+use gtk::glib::{GString, SignalHandlerId};
+use gtk::Window;
 use gtk::{Label, ListItem, ListView, NoSelection, SignalListItemFactory, Stack, StringList, StringObject, Box, gio::File};
 use gtk::{prelude::*, Button, FileDialog, FileFilter, gio::ListStore, Builder};
 use gtk::{glib, glib::clone, Application, ApplicationWindow};
@@ -18,12 +22,139 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 
+struct FileList {
+    string_list: Arc<StringList>,
+    model: Arc<NoSelection>,
+    factory: SignalListItemFactory,
+    signals: Arc<Mutex<Vec<SignalHandlerId>>>,
+    listview: ListView,
+}
+
+impl FileList {
+    fn new(lv: ListView) -> FileList {
+        let mut fl = FileList {
+            listview: lv,
+            string_list: Arc::new(StringList::new(&[])),
+            model: Arc::new(NoSelection::new(None::<StringList>)),
+            factory: SignalListItemFactory::new(),
+            signals: Arc::new(Mutex::new(Vec::new())),
+        };
+        fl.model.set_model(Some(fl.string_list.as_ref()));
+        fl.factory.connect_setup(move |_, obj| {
+            let bx = Box::builder()
+                .orientation(gtk::Orientation::Horizontal)
+                .spacing(5)
+                .build();
+            let lbl = Label::new(None);
+            bx.append(&lbl);
+            let delbut = Button::builder()
+                .label("-")
+                .build();
+            bx.append(&delbut);
+            obj
+                .downcast_ref::<ListItem>()
+                .expect("Needs to be ListItem")
+                .set_child(Some(&bx));
+        });
+
+        fl.factory.connect_bind(clone!(
+            #[strong(rename_to = sl)] fl.string_list,
+            #[strong(rename_to = signals)] fl.signals,
+            move |_, obj| {
+            let item = obj
+                .downcast_ref::<ListItem>()
+                .expect("Needs to be ListItem");
+            let pathobj = item.item()
+                .and_downcast::<StringObject>()
+                .expect("The item has to be an `IntegerObject`.");
+            let bx = obj
+                .downcast_ref::<ListItem>()
+                .expect("Needs to be ListItem")
+                .child()
+                .and_downcast::<Box>()
+                .expect("The child has to be a `Box`.");
+            let label = bx.first_child()
+                .expect("Can't get first child")
+                .downcast::<Label>()
+                .expect("Can't downcast to Label");
+            label.set_text(&pathobj.string());
+
+            let delbut = bx.last_child()
+                .expect("Can't get last child")
+                .downcast::<Button>()
+                .expect("Can't downcast to Button");
+            let sid = delbut.connect_clicked(clone!(
+            #[strong] sl,
+            #[strong] item,
+            move |_| {
+                let item_index = item.position();
+                //println!("I'm deleting {item_index}!!!");
+                sl.remove(item_index);
+            }));
+            let mut sig = signals.lock().unwrap();
+            let index = item.position() as usize;
+            if index >= sig.len() {
+                sig.push(sid);
+            } else {
+                sig[index] = sid;
+            }
+        }));
+
+        fl.factory.connect_unbind(clone!(
+        #[strong(rename_to = signals)] fl.signals,
+        #[strong(rename_to = strlist)] fl.string_list,
+        move |_, obj| {
+            let item = obj
+                .downcast_ref::<ListItem>()
+                .expect("Needs to be ListItem");
+            let bx = obj
+                .downcast_ref::<ListItem>()
+                .expect("Needs to be ListItem")
+                .child()
+                .and_downcast::<Box>()
+                .expect("The child has to be a `Box`.");
+            let label = bx.first_child()
+                .expect("Can't get first child")
+                .downcast::<Label>()
+                .expect("Can't downcast to Label");
+            let delbut = bx.last_child()
+                .expect("Can't get last child")
+                .downcast::<Button>()
+                .expect("Can't downcast to Button");
+            let mut sig = signals.lock().unwrap();
+            //println!("{sig:?}");
+            let id = item.position() as usize;
+            if id >= sig.len() {
+                println!("{id}/{} {} out of bounds", sig.len(), label.text());
+                return;
+            }
+            delbut.disconnect(sig.remove(id));
+            let mut i = 0;
+            while i < strlist.n_items() {
+                if strlist.string(i) == Some(label.text()) {
+                    println!("Real index: {i}");
+                }
+                i += 1;
+            }
+            println!("Deleted a signal handler {}: {}", label.text(), item.position());
+        }));
+
+        fl.listview.set_factory(Some(&fl.factory));
+        fl.listview.set_model(Some(fl.model.as_ref()));
+        return fl;
+    }
+
+    fn add_file(&self, path: String) {
+        println!("file: {path}");
+        self.string_list.append(&path);
+    }
+}
+
 fn build_ui(app: &Application) {
-    let button = Button::builder()
-        .label("Open file")
-        .halign(gtk::Align::Center)
-        .valign(gtk::Align::Center)
-        .build();
+    let b = Builder::from_file("./ui.ui");
+    let button = b.object::<Button>("open_file_button")
+        .expect("Couldn't find button");
+
     let lm = ListStore::new::<FileFilter>();
     let ff = FileFilter::new();
     ff.add_suffix("png");
@@ -32,85 +163,25 @@ fn build_ui(app: &Application) {
         .filters(&lm)
         .build();
 
-    let main_stack = Arc::new(Stack::new());
-    main_stack.add_child(&button);
+    let main_stack = Arc::new(b.object::<Stack>("main_stack").expect("Stack not found"));
 
-    let modellist = Arc::new(StringList::new(&[]));
-    let model = Arc::new(NoSelection::new(Some((*modellist).clone())));
-    let factory = SignalListItemFactory::new();
-    factory.connect_setup(move |_, obj| {
-        let bx = Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(5)
-            .build();
-        let lbl = Label::new(None);
-        bx.append(&lbl);
-        let delbut = Button::builder()
-            .label("-")
-            .build();
-        bx.append(&delbut);
-        obj
-            .downcast_ref::<ListItem>()
-            .expect("Needs to be ListItem")
-            .set_child(Some(&bx));
-    });
-
-    factory.connect_bind(clone!(
-        #[strong] modellist,
-        move |_, obj| {
-        let item = obj
-            .downcast_ref::<ListItem>()
-            .expect("Needs to be ListItem");
-        let item_index = item.position();
-        let pathobj = item.item()
-            .and_downcast::<StringObject>()
-            .expect("The item has to be an `IntegerObject`.");
-        let bx = obj
-            .downcast_ref::<ListItem>()
-            .expect("Needs to be ListItem")
-            .child()
-            .and_downcast::<Box>()
-            .expect("The child has to be a `Box`.");
-        let label = bx.first_child()
-            .expect("Can't get first child")
-            .downcast::<Label>()
-            .expect("Can't downcast to Label");
-        label.set_text(&pathobj.string());
-
-        let delbut = bx.last_child()
-            .expect("Can't get last child")
-            .downcast::<Button>()
-            .expect("Can't downcast to Button");
-        delbut.connect_clicked(clone!(
-        #[strong] modellist,
-        move |_| {
-            modellist.remove(item_index);
-        }));
-    }));
-
-    let file_list = ListView::builder()
-        .factory(&factory)
-        .model(model.as_ref())
-        .build();
-
-    main_stack.add_named(&file_list, Some("file_list"));
+    let file_list = b.object::<ListView>("file_list").expect("file_list not found");
+    let fl: Arc<FileList> = Arc::new(FileList::new(file_list));
 
     // Create a window and set the title
-    let window: ApplicationWindow = ApplicationWindow::builder()
-        .application(app)
-        .title("Uzanti Cevirmeni")
-        .default_width(500)
-        .default_height(600)
-        .child(main_stack.as_ref())
-        .build();
-    let warc: Arc<ApplicationWindow> = Arc::new(window.clone());
-    button.connect_clicked(move |button| {
-        let w = warc.clone();
-        let s = main_stack.clone();
-        let sl = modellist.clone();
-        button.set_label("Clicked!");
+    let window = Arc::new(b.object::<Window>("main_window")
+        .expect("Couldn't find window"));
+
+    button.connect_clicked(clone!(
+    #[strong] window,
+    #[strong] main_stack,
+    #[strong] fl,
+    move |_| {
         let cancel = Cancellable::new();
-        cc.open_multiple(Some(w.as_ref()), Some(&cancel), clone!(
+        println!("Pressed button!");
+        cc.open_multiple(Some(window.as_ref()), Some(&cancel), clone!(
+            #[strong] main_stack,
+            #[strong] fl,
             move |res| {
                 let files = res.expect("Opening file dialog failed");
                 let mut i = 0;
@@ -119,15 +190,15 @@ fn build_ui(app: &Application) {
                     let file = b
                         .downcast_ref::<File>()
                         .expect("Couldn't downcast to File");
-                    println!("file: {}", file.path().unwrap().into_os_string().into_string().unwrap());
-                    s.set_visible_child_name("file_list");
-                    sl.append(&file.path().unwrap().into_os_string().into_string().unwrap());
+                    main_stack.set_visible_child_name("file_list");
+                    fl.add_file(String::from(file.path().expect("Can't get path").to_str().unwrap()));
                     i += 1;
                 }
             }
         ));
-    });
+    }));
 
     // Present window
+    window.set_application(Some(app));
     window.present();
 }
